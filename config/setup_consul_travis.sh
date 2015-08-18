274139aab4420d531da552ea25043c53a0d9dd0a
eunice <eunicekokor@users.noreply.github.com>
2015-08-18 01:01:40 -0700

    Update setup_consul_travis.sh

--- config/setup_consul_travis.sh
+++ config/setup_consul_travis.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 consul_set() {
-	curl -X PUT -d "$2" http://localhost:8500/v1/kv/adi-website/$1
-	curl http://localhost:8500/v1/kv/adi-website/$1
+	cat "gothere"
 }
 
 # Flask configurations
